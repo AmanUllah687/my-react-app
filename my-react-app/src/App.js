12158b7ae2528b566ce73ea16122e2dc767c2e71
Aman Ullah <Amanch687@gmail.com>
2026-01-25 18:31:46 +0500

    Setting State triggers renders

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,27 @@
-import Clock from "./Clock";
-export default function App ({time}) {
+import { useState } from 'react';
+ export default function Form () {
+  const[isSent, setIsSent] = useState(false);
+  const[message, setMessage] = useState('Hi!');
+  if (isSent) {
+    return <h1> Your Message is  on its way.</h1>
+  }
+
   return (
-    
-    <Clock time={new Date()} />
+  <form onSubmit={(e) => {
+    e.preventDefault();
+    setIsSent(true);
+    setMessage(message);
+  }}>
+    <textarea
+    placeholder='message'
+    value={message}
+    onChange={e => setMessage(e.target.value)}
+    />
+    <button type="submit">Send</button>
+
+  </form>
   );
-}
+ }
+ function sendMessage(message) {
+  //...
+ }
