8b15e10bbd7f9bac28369ade365924bc1c1db56e
Aman Ullah <Amanch687@gmail.com>
2026-01-26 23:25:58 +0500

    Renedring Takes a Snapshot in Time

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,27 +1,14 @@
-import { useState } from 'react';
- export default function Form () {
-  const[isSent, setIsSent] = useState(false);
-  const[message, setMessage] = useState('Hi!');
-  if (isSent) {
-    return <h1> Your Message is  on its way.</h1>
-  }
-
+import {useState} from 'react';
+export default function Counter () {
+  const [number, setNumber] = useState(0);
   return (
-  <form onSubmit={(e) => {
-    e.preventDefault();
-    setIsSent(true);
-    setMessage(message);
-  }}>
-    <textarea
-    placeholder='message'
-    value={message}
-    onChange={e => setMessage(e.target.value)}
-    />
-    <button type="submit">Send</button>
-
-  </form>
+    <>
+    <h1>{number}</h1>
+    <button onClick={() => {
+      setNumber(number + 1);
+      setNumber(number + 1);
+      setNumber(number + 1);
+    }}>+3</button>
+    </>
   );
- }
- function sendMessage(message) {
-  //...
- }
+}
