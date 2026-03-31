1b98844ab98ae1ef1ddf4c8d90ca08363cc36da6
Aman Ullah <Amanch687@gmail.com>
2026-03-31 19:40:20 +0500

    Setup for Developing the tic-tac-toe

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,16 +1,3 @@
-import { useState } from 'react';
-
-export default function Counter() {
-  const [number, setNumber] = useState(0);
-
-  return (
-    <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(n => n + 1);
-        setNumber(n => n + 1);
-        setNumber(n => n + 1);
-      }}>+3</button>
-    </>
-  )
+export default function Square () {
+  return <button className="square">X</button>
 }
