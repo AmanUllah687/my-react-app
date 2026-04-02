08648a36a04ddf03f76184359c2e455be8af944f
Aman Ullah <Amanch687@gmail.com>
2026-04-02 15:40:12 +0500

    Adding Time Travel(Storing the history of the moves)

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,9 @@
-import { useState } from "react";
+import {useState } from "react";
 function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
-export default function Board () {
-  const[xIsNext, setXIsNext] = useState(true);
-  const[squares, setSquares] = useState(Array(9).fill(null));
+function Board ({xIsNext, squares, onPlay}) {
+  
    function handleClick(i) {
     if (squares[i] || calculateWiner(squares)) {
       return;
@@ -15,8 +14,8 @@ export default function Board () {
     } else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+   onPlay(nextSquares);
+    
   }
     const winner = calculateWiner(squares);
     let status;
@@ -46,6 +45,27 @@ export default function Board () {
   </div>
   
   </>
+  )
+}
+export default function Game() {
+  const[xIsNext, setXIsNext] = useState(true);
+  const[history,setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length-1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext}  squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{/*To Do*/}</ol>
+      </div>
+    </div>
   )
 }
 
