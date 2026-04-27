10a676b3c463f53946246b60eed1f7b74a4b519f
Aman Ullah <Amanch687@gmail.com>
2026-04-27 19:16:20 +0500

    Rendering lists Better

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,23 +2,41 @@ import { people } from "./data";
 import { getImageUrl } from "./utils";
 
 export default function List() {
-  const listItems =  people.map(person =>
-    <li key={person.id}>
-      <img 
-      src = {getImageUrl(person)}
-      alt={person.name}
-      />
-      <p>
-        <b>{person.name}</b>
-        {'' + person.profession + ""}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  )
+  const chemist = people.filter(person => 
+    person.profession === 'chemist'
+  );
+  const everyOneElse = people.filter(person => 
+    person.profession !== 'chemist'
+  );
   return (
     <article>
       <h1>scientists</h1>
-      <ul>{listItems}</ul>
+      <h2>Chemists</h2>
+      <ul>{chemist.map(person =>
+        <li key={person.id}>
+        <img 
+        src={getImageUrl(person)}
+        alt={person.name} />
+        <p>
+          <b>{person.name}:</b>
+          {"" + person.profession + "" } 
+          known for {person.accomplishment}
+        </p>
+        </li>
+      )}</ul>
+      <h2>everyOneElse</h2>
+      <ul>{everyOneElse.map(person =>
+        <li key={person.id}>
+        <img 
+        src={getImageUrl(person)}
+        alt={person.name} />
+        <p>
+          <b>{person.name}:</b>
+          {"" + person.profession + "" } 
+          known for {person.accomplishment}
+        </p>
+        </li>
+      )}</ul>
     </article>
   )
 }
