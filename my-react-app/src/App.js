93f5a40ca2787371d4ebe73a4f8d31d9112a5e99
Aman Ullah <Amanch687@gmail.com>
2026-04-28 19:58:58 +0500

    Local mutation: Your component’s little secret

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,11 +3,9 @@ function Cup({guest}) {
 }
 
 export default function TeaSet() {
-  return (
-    <>
-    <Cup  guest={1}/>
-    <Cup  guest={2}/>
-    <Cup  guest={3}/>
-    </>
-  )
+  const cups = [];
+  for(let i=1; i<=12;i++) {
+    cups.push(<Cup key={i} guest={i} />);
+  }
+  return cups;
 }
