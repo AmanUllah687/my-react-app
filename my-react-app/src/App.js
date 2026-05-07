e686ca983ac2ab9ed5379d7544ab7d8a2808ab77
Aman Ullah <Amanch687@gmail.com>
2026-05-07 16:56:24 +0500

    Removing from an array using filter() Method

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,25 +1,26 @@
-import { useState } from 'react';
-let nextId = 0;
- export default function List()  {
-    const[name, setName] = useState('');
-    const[artists, setArtists] = useState([]);
-    return (
+import {useState } from 'react';
+let intialArtists = [
+  { id: 0, name: 'Marta Colvin Andrade' },
+  { id: 1, name: 'Lamidi Olonade Fakeye'},
+  { id: 2, name: 'Louise Nevelson'},
+ ]
+ export default function List() {
+    const[artists, setArtists] = useState(intialArtists);
+    return(
         <>
-        <h1>Inspiring Sculputures: </h1>
-        <input value={name} 
-        onChange={e => setName(e.target.value)} 
-        />
-        <button onClick={() => {
-            setArtists([
-                ...artists,
-                { id: nextId++, name: name}
-            ]);
-        }}>Add</button>
-            <ul>
-                {artists.map(artist => (
-                    <li key={artist.id}>{artist.name}</li>
-                ))}
-            </ul>
+        <h1>Inspiring Sculptors</h1>
+        <ul>
+            {artists.map(artist => (
+                <li key={artist.id}>
+                    {artist.name}{''}
+                    <button onClick={() => {
+                        setArtists(artists.filter(a =>
+                            a.id !== artist.id
+                        ))
+                    }}>Delete</button>
+                </li>
+            ))}
+        </ul>
         </>
     );
  }
