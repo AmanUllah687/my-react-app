02c2f5163862b249f77bbf63fce21fda5f0759c2
Aman Ullah <Amanch687@gmail.com>
2026-05-07 20:38:41 +0500

    Replacing items in an array using Map()

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,73 +1,33 @@
 import { useState } from "react";
- let initialShapes = [
-    { id: 0, type: 'circle', x: 50, y: 100 },
-    { id: 1, type: 'square', x: 150, y: 100 },
-    { id: 2, type: 'circle', x: 250, y: 100 },
- ];
-  export default function ShapeEditor() {
-    const[shapes, setShapes] = useState(
-        initialShapes
+let initialCounters = [
+    0,0,0
+];
+export default function CounterList() {
+    const[counters, setCounters] =useState(
+        initialCounters
     );
-    function handleClick() {
-        const nextShapes = shapes.map(shape => {
-            if(shape.type === 'square') {
-               return shape; 
+    function handleIncrementClick(index) {
+        const nextCounters = counters.map((c,i) => {
+            if(i === index) {
+                return c + 1;
             } else {
-                return {
-                    ...shape,
-                    y: shape.y + 50,
-                };
+                return c;
             }
         });
-        setShapes(nextShapes)
-    }
-    function handleUpClick() {
-        const nextShapes = shapes.map(shape => {
-            if(shape.type === 'square') {
-               return shape; 
-            } else {
-                return {
-                    ...shape,
-                    y: shape.y - 50,
-                };
-            }
-        });
-        setShapes(nextShapes)
+        setCounters(nextCounters);
     }
     return(
         <>
-        <button onClick={handleClick}>
-            Move Circles Down!
-        </button>
-        {shapes.map(shape => (
-            <div
-            key={shape.id}
-            style={{
-                background: 'purple',
-                position: "absolute",
-                left: shape.x,
-                top: shape.y,
-                borderRadius: shape.type === 'circle' ? '50%' : '',
-                width: 20,
-                height: 20,
-            }}></div>
-        ))}
-        <button onClick={handleUpClick}>
-            Move Circles Up!
-        </button>
-        {shapes.map(shape => (
-            <div
-            key={shape.id}
-            style={{
-                background: 'purple',
-                position: "absolute",
-                left: shape.x,
-                top: shape.y,
-                borderRadius: shape.type === 'circle' ? '50%' : '',
-                width: 20,
-                height: 20,
-            }}></div>
-        ))}
+        <ul>
+            {counters.map((counter, i) => (
+                <li key={i}>
+                    {counter}
+                    <button onClick={() => {
+                        handleIncrementClick(i);
+                    }}>+1</button>
+                </li>
+            ))}
+        </ul>
         </>
     );
-  }
+}
