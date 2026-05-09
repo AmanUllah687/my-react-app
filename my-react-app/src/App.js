5e930d7210023aab547a64bc81b7a59e556aa4b4
Aman Ullah <Amanch687@gmail.com>
2026-05-09 17:52:07 +0500

    Reversing an Array in React

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,40 +1,26 @@
 import { useState } from "react";
-let nextId = 3;
-const initailArts =  [
-  { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+const initialList = [
+  { id: 0, title: 'Big Bellies' },
+  { id: 1, title: 'Lunar Landscape' },
+  { id: 2, title: 'Terracotta Army' },
 ];
  export default function List () {
-    const[name, setName] = useState('');
-    const[artists, setArtists] = useState(
-        initailArts
+    const[list, setList] = useState(initialList);
+    function handleClick() {
+        const nextList = [...list];
+        nextList.reverse();
+        setList(nextList);
+    }
+    return(
+        <>
+        <button onClick={handleClick} >
+            Reverse
+        </button>
+        <ul>
+            {list.map(artwork => (
+                <li key={artwork.id}>{artwork.title}</li>
+            ))}
+            </ul>        
+        </>
     );
- function handleClick() {
-    const insertAt = 1;
-    const nextArtists = [
-        ...artists.slice(0,insertAt),
-        {id: nextId++, name: name},
-        ...artists.slice(insertAt)
-    ];
-    setArtists(nextArtists);
-    setName('');
-}
-
-return (
-    <>
-    <h1>Inspiring Sculputures: </h1>
-    <input 
-    value={name} 
-    onChange={e => setName(e.target.value)} />
-    <button onClick={handleClick}>
-        Insert
-    </button>
-    <ul>
-        {artists.map(artist => (
-            <li key={artist.id}>{artist.name}</li>
-        ))}
-    </ul>
-    </>
-  );
-}
+ }
