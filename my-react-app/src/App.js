0415ecd30c716e26ce7c860ee12a144187374a71
Aman Ullah <Amanch687@gmail.com>
2026-05-10 11:57:32 +0500

    Updating objects inside arrays

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,65 @@
 import { useState } from "react";
+let  nextId = 3;
 const initialList = [
-  { id: 0, title: 'Big Bellies' },
-  { id: 1, title: 'Lunar Landscape' },
-  { id: 2, title: 'Terracotta Army' },
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
 ];
- export default function List () {
-    const[list, setList] = useState(initialList);
-    function handleClick() {
-        const nextList = [...list];
-        nextList.reverse();
-        setList(nextList);
+ export default function BucketList () {
+    const[myList, setMyList] = useState(initialList);
+    const[yourList, setYourList] = useState(initialList);
+    function handleToggleMyList (artworkId, nextSeen) {
+        const myNextList = [...myList];
+        const artwork = myNextList.find(
+            a => a.id === artworkId 
+        );
+        artwork.seen = nextSeen;
+        setMyList(myNextList);
     }
-    return(
+     function handleToggleYourList (artworkId, nextSeen) {
+        const yourNextList = [...yourList];
+        const artwork = yourNextList.find(
+            a => a.id === artworkId 
+        );
+        artwork.seen = nextSeen;
+        setYourList(yourNextList);
+    }
+    return (
         <>
-        <button onClick={handleClick} >
-            Reverse
-        </button>
-        <ul>
-            {list.map(artwork => (
-                <li key={artwork.id}>{artwork.title}</li>
-            ))}
-            </ul>        
+        <h1>My Art Bucket List</h1>
+        <h2>MY list of Art to see:</h2>
+        <ItemList 
+        artworks={myList}
+        onToggle={handleToggleMyList} />
+        <h2>Your List of art to see:</h2>
+        <ItemList
+        artworks={yourList}
+        onToggle={handleToggleYourList}  />
         </>
+    );
+ } 
+ function ItemList({artworks, onToggle}) {
+    return ( 
+        <ul>
+            {artworks.map(artwork => (
+                <li key={artwork.id}>
+                    <label>
+                        <input 
+                        type="checkbox"
+                        checked={artwork.seen}
+                        onChange={e => {
+                            onToggle(
+                                artwork.id,
+                                e.target.checked
+
+                            );
+                        }}
+                        />
+                        {artwork.title}
+                    </label>
+
+                </li>
+            ))}
+        </ul>
     );
  }
