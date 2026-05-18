0fd989f72113a0efe39f68f4d7c67f1c9aa993fe
Aman Ullah <Amanch687@gmail.com>
2026-05-18 12:27:10 +0500

    Example: building a stopwatch using UseRef

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,33 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
-export default function Counter() {
-    let ref = useRef(0);
-    function handleClick() {
-        ref.current = ref.current + 1;
-        alert('You Clicked' +  ref.current +  'Times!.');
+export default function StopWatch() {
+    const[starTime, setStartTime] = useState(null);
+    const[now, setNow] = useState(null);
+    const intervalRef = useRef(null);
+    function handleStart() {
+        setStartTime(Date.now());
+        setNow(Date.now());
+        clearInterval(intervalRef.current)
+       intervalRef.current = setInterval(() => {
+        setNow(Date.now)},10);
     }
-    return(
-        <button onClick={handleClick}>
-            Click me!
+    function handleStop() {
+        clearInterval(intervalRef.current);
+    }
+    let secondPassed = 0;
+    if(starTime != null && now != null) {
+        secondPassed = (now - starTime)/ 1000;
+
+    }
+    return (
+        <>
+        <h1>Time Passed: {secondPassed.toFixed(3)}</h1>
+        <button onClick={handleStart}>
+            Start
         </button>
+        <button onClick={handleStop}>
+            Stop
+        </button>
+        </>
     );
 }
