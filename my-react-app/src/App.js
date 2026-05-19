ad3019fa113758c3b994dbbdb80c92cd2b3912cb
Aman Ullah <Amanch687@gmail.com>
2026-05-19 19:50:46 +0500

    How to manage a list of refs using a ref callback

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,66 +1,71 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import  './App.css';
 
 export default function CatFriends() {
-    const firstCatRef = useRef(null);
-    const secondCatRef = useRef(null);
-    const thirdCatRef = useRef(null);
-    function handleScrollToFirstCat() {
-        firstCatRef.current.scrollIntoView({
+    const itemsRef = useRef(null);
+    const [catList, setCatList] = useState(setupCatList);
+
+    function scrollToCat(cat) {
+        const map = getMap();
+        const node = map.get(cat);
+            node.scrollIntoView({
             behavior: 'smooth',
             block: 'nearest',
             inline: 'center'
 
         });
     }
-     function handleScrollToSecondCat() {
-        secondCatRef.current.scrollIntoView({
-            behavior: 'smooth',
-            block: 'nearest',
-            inline: 'center'
-
-        });
+     function getMap() {
+        if(!itemsRef.current) {
+            itemsRef.current = new Map();
+        }
+        return itemsRef.current;
     }
-     function handleScrollToThirdCat() {
-        thirdCatRef.current.scrollIntoView({
-            behavior: 'smooth',
-            block: 'nearest',
-            inline: 'center'
-
-        });
-    }
+     
+    
     return(
         <>
         <nav>
-            <button onClick={handleScrollToFirstCat}>Neo</button>
-            <button onClick={handleScrollToSecondCat}>Millie</button>
-            <button onClick={handleScrollToThirdCat}>Bella</button>
+            <button onClick={() => scrollToCat(catList[0])}>Neo</button>
+            <button onClick={() => scrollToCat(catList[5])}>Millie</button>
+            <button onClick={() => scrollToCat(catList[8])}>Bella</button>
         </nav>
         <div>
             <ul>
-                <li>
-                    <img
-              src="https://placecats.com/neo/300/200"
-              alt="Neo"
-              ref={firstCatRef}
-            />
+              {catList.map((cat) => (
+                <li key={cat.id} 
+                    ref={(node) => {
+                        const map = getMap();
+                        map.set(cat, node);
+                        return () => {
+                            map.delete(cat);
+                        }
+                    }}
+                >
+                    <img src={cat.imageUrl} />
                 </li>
-                <li>
-                    <img
-              src="https://placecats.com/millie/200/200"
-              alt="Millie"
-              ref={secondCatRef}
-            />
-                </li>
-                <li>
-                    <img
-              src="https://placecats.com/bella/199/200"
-              alt="Bella"
-              ref={thirdCatRef}
-            />
-                </li>
+              ))}
             </ul>
         </div>
         </>
     );
+}
+function setupCatList() {
+    const catCount = 10;
+    const catList = new Array(catCount)
+    for(let i=0;i<catCount; i++) {
+        let imageUrl = '';
+        if(i<5) {
+            imageUrl = "https://placecats.com/neo/320/240";
+        } else if(i<8) {
+            imageUrl = "https://placecats.com/millie/320/240";
+        } else {
+            imageUrl = "https://placecats.com/bella/320/240";
+        }
+        catList[i] = {
+            id: i,
+            imageUrl
+        };
+    }
+    return catList;
 }
