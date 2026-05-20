9b4dea8d20a962ccd94356a7ca1d219acd34d730
Aman Ullah <Amanch687@gmail.com>
2026-05-20 16:25:07 +0500

    Flushing state updates synchronously with flushSync

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,22 +1,47 @@
-import { useRef, useImperativeHandle } from "react";
- function MyInput({ref}) {
-    const realInputRef = useRef(null);
-    useImperativeHandle(ref, () =>({
-        focus() {
-            realInputRef.current.focus();
-        }
-    }))
-    return <input ref={realInputRef} />
- }
- export default function Form() {
-    const inputRef = useRef(null);
-    function handleClick() {
-        inputRef.current.focus();
+import { useState, useRef } from 'react';
+import { flushSync } from 'react-dom';
+
+export default function TodoList () {
+    const listRef = useRef(null);
+    const[text, setText] = useState('');
+    const[todos, setTodos] = useState(
+        initialTodos
+    );
+    function handleAdd() {
+        const newTodo = {id: nextId++, text: text};
+        flushSync(() => {
+      setText('');
+      setTodos([ ...todos, newTodo]);
+    });
+        listRef.current.lastChild.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest'
+        })
     }
-    return (
+    return(
         <>
-        <MyInput ref={inputRef} />
-        <button on onClick={handleClick}>Focus the input:</button>
-            </>
-    )
-  }
+        <button onClick={handleAdd}>
+            Add
+        </button>
+        <input 
+        value={text}
+        onChange={e => setText(e.target.value)}
+        />
+        <ul ref={listRef}>
+          {todos.map(todo => (
+            <li key={todo.id}>
+                {todo.text}
+            </li>
+          ))}
+        </ul>
+        </>
+    );
+}
+let nextId = 0;
+let initialTodos = [];
+for(let i=0;i<20;i++) {
+    initialTodos.push({
+        id: nextId++,
+        text: 'Todo #' +  (i+1)
+    })
+}
