fc2bd65026e2257401a6d99b835e2d0a83c4d498
Aman Ullah <Amanch687@gmail.com>
2026-05-20 19:44:53 +0500

    Avoid changing DOM nodes managed by React. Modifying, adding children to, or removing children from elements that are managed by React can lead to inconsistent visual results or crashes like above.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,47 +1,21 @@
-import { useState, useRef } from 'react';
-import { flushSync } from 'react-dom';
+import { useState, useRef } from "react";
 
-export default function TodoList () {
-    const listRef = useRef(null);
-    const[text, setText] = useState('');
-    const[todos, setTodos] = useState(
-        initialTodos
-    );
-    function handleAdd() {
-        const newTodo = {id: nextId++, text: text};
-        flushSync(() => {
-      setText('');
-      setTodos([ ...todos, newTodo]);
-    });
-        listRef.current.lastChild.scrollIntoView({
-            behavior: 'smooth',
-            block: 'nearest'
-        })
-    }
+export default function Counter() {
+    const[show, setshow] = useState(true);
+    const ref = useRef(null);
     return(
-        <>
-        <button onClick={handleAdd}>
-            Add
-        </button>
-        <input 
-        value={text}
-        onChange={e => setText(e.target.value)}
-        />
-        <ul ref={listRef}>
-          {todos.map(todo => (
-            <li key={todo.id}>
-                {todo.text}
-            </li>
-          ))}
-        </ul>
-        </>
+        <div>
+            <button onClick={() =>
+                setshow(!show)
+            }>
+                Toggle with setState
+            </button>
+            <button onClick={() => {
+                ref.current.remove();
+                }} >
+                     Remove from the DOM
+                </button>
+                { show && <p ref={ref}>Hello World</p>}
+        </div>
     );
-}
-let nextId = 0;
-let initialTodos = [];
-for(let i=0;i<20;i++) {
-    initialTodos.push({
-        id: nextId++,
-        text: 'Todo #' +  (i+1)
-    })
 }
