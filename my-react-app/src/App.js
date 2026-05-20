5a1ee81758def4f821d67d38bbc2ba3cab1e8896
Aman Ullah <Amanch687@gmail.com>
2026-05-20 21:55:14 +0500

    Scrolling an image carousel Using flushSync()

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,21 +1,77 @@
 import { useState, useRef } from "react";
+import { flushSync } from 'react-dom';
+import './App.css'
 
-export default function Counter() {
-    const[show, setshow] = useState(true);
-    const ref = useRef(null);
+export default function CatFriends() {
+    const selectedRef = useRef(null);
+    const[index, setIndex] = useState(0);
     return(
-        <div>
-            <button onClick={() =>
-                setshow(!show)
-            }>
-                Toggle with setState
-            </button>
+        <>
+        <nav>
             <button onClick={() => {
-                ref.current.remove();
-                }} >
-                     Remove from the DOM
-                </button>
-                { show && <p ref={ref}>Hello World</p>}
+                flushSync(() => {
+                    if(index < catList.length - 1) {
+                        setIndex(index + 1);
+                    } else{
+                        setIndex(0)
+                    }
+                });
+                selectedRef.current.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'nearest',
+                    inline: 'center'
+               });
+            }}>
+                Next
+            </button>
+        </nav>
+        <div>
+            <ul>
+                {catList.map((cat, i) => (
+                    <li key={cat.id}
+                         ref={index === i ?
+                         selectedRef :
+                         null}
+                    >
+                        <img
+                className={
+                  index === i ?
+                    'active'
+                    : ''
+                }
+                src={cat.imageUrl}
+                alt={'Cat #' + cat.id}
+              />
+                    </li>
+
+                ))}
+            </ul>
         </div>
+        </>
     );
+}
+const catCount = 10;
+const catList = new Array(catCount);
+for(let i=0; i< catCount; i++) {
+    const bucket = Math.floor(Math.random() + catCount) % 2;
+    let imageUrl = '';
+    switch(bucket) {
+        case 0: {
+            imageUrl = "https://placecats.com/neo/250/200";
+            break;
+        }
+        case 1: {
+      imageUrl = "https://placecats.com/millie/250/200";
+      break;
+    }
+    case 2:
+    default: {
+      imageUrl = "https://placecats.com/bella/250/200";
+      break;
+    }
+    }
+    catList[i] = {
+        id: i,
+        imageUrl
+    };
 }
