fbad5ae0288e5c4c82d5c94efb09a4f001eb86f4
Aman Ullah <Amanch687@gmail.com>
2026-05-23 22:18:19 +0500

    getting to know how Effects Work in Practice

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,41 @@
-import { useEffect } from "react";
-import { createConnection } from './Chat.js';
+import { useState, useEffect} from "react";
+function Playground() {
+    const[text, setText] = useState('a');
 
-export default function ChatRoom() {
     useEffect(() => {
-        const connection = createConnection();
-        connection.connect();
-        return () => connection.disconnect();
-    }, []);
-    return <h1>Welcome to the Chat!</h1>
+        function onTimeout() {
+            console.log('⏰ ' + text);
+        }
+        console.log('🔵 Schedule "' + text + '" log');
+        const timeoutId = setTimeout(onTimeout, 3000);
+        
+       // return () => {
+         //   console.log('🟡 Cancel "' + text + '" log');
+           // clearTimeout(timeoutId);
+        //}
+    }, [text]);
+    return(
+        <>
+        <label>
+            What to log:{' '}
+            <input 
+            value={text}
+            onChange={e => setText(e.target.value)}
+            />
+        </label>
+        <h1>{text}</h1>
+        </>
+    );
+}
+export default function App() {
+    const[show, setShow] = useState(false);
+    return(
+        <>
+        <button onClick={() => setShow(!show)}>
+            {show ? 'Unmount' : 'Mount'} the component
+        </button>
+        { show && <hr />}
+        {show && <Playground />}
+        </>
+    )
 }
