b2c114c2bcd7a2f618c0757ca0b24d7be373603a
Aman Ullah <Amanch687@gmail.com>
2026-06-01 15:06:32 +0500

    Undesrtansding and Simplifing the useState and useEffect hooks

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,32 +1,45 @@
-import { useState, useEffect } from "react";
-import { fetchBio } from './api.js';
- 
-export default function Page() {
-    const[person, setPerson] = useState('Alice');
-    const[bio, setBio] = useState(null);
-     useEffect(() => {
-        let ignore = false;
-        setBio(null);
-        fetchBio(person).then(result => {
-            if(!ignore) {
-                setBio(result);
-            }
-        });
-        return () => {
-            ignore = true;
-        }
-     }, [person]);
-     return(
+import { useState } from "react";
+import { initialTodos, createTodo } from "./todos.js";
+export default function TodoList() {
+    const[todos, setTodos] = useState(initialTodos);
+    const[showActive, setShowActive] = useState(false);
+    const activeTodos = todos.filter(todo => !todo.completed);
+    const visibleTodos = showActive ? activeTodos : todos;
+    return(
         <>
-        <select value={person} onChange={e => setPerson(e.target.value)}>
-            <option value="Alice">Alice</option>
-            <option value="Bob">Bob</option>
-            <option value="Taylor">Taylor</option>
-        </select>
-        <hr />
-        <p>
-            <i>{bio ?? 'Loading...'}</i>
-        </p>
+        <label>
+            <input
+            type="checkbox"
+            checked={showActive}
+            onChange={e => setShowActive(e.target.checked)}
+            />
+        </label>
+        <NewTodo onAdd={newTodo => setTodos([...todos, newTodo])}/>
+            <ul>
+                {visibleTodos.map(todo => (
+                    <li key={todo.id}>
+                        {todo.completed ? <s>{todo.text}</s> : todo.text}
+                    </li>
+                ))}
+            </ul>
+            <footer>
+                {activeTodos.length} todos left
+            </footer>
         </>
-     ); 
+    ) 
+}
+function NewTodo({onAdd}) {
+    const[text, setText] = useState('');
+    function handleAddClick() {
+        setText('');
+        onAdd(createTodo(text));
+    }
+    return(
+        <>
+        <input value={text} onChange={e => setText(e.target.value)} />
+        <button onClick={handleAddClick}>
+            Add
+        </button>
+        </>
+    );
 }
